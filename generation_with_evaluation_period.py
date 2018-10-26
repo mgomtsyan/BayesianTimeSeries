6ecabc4f87f3bf1a4698db8f7f9ff484a3300cbc
dashademidova <35659527+dashademidova@users.noreply.github.com>
2018-10-26 05:34:14 +0300

    Update generation_with_evaluation_period.py

--- generation_with_evaluation_period.py
+++ generation_with_evaluation_period.py
@@ -8,7 +8,7 @@ import pyro.distributions as dist
 
 from torch.distributions.normal import Normal
 
-def generative_procedure_with_eval(mu_0, delta_0, gamma_0, sigma_e, sigma_o, sigma_u, sigma_r, sigma_v, sigma_w, p_a, p_c, L, L_train, S, t_c_fixed, c_fixed, t_r_fixed, r_fixed):
+def generative_procedure_with_evaluation_period(mu_0, delta_0, gamma_0, sigma_e, sigma_o, sigma_u, sigma_r, sigma_v, sigma_w, p_a, p_c, L, L_train, S, t_c_fixed, c_fixed, t_r_fixed, r_fixed):
     
     # 1. Generate anomalies and change points indexes
     
@@ -26,12 +26,12 @@ def generative_procedure_with_eval(mu_0, delta_0, gamma_0, sigma_e, sigma_o, sig
     z_c[t_c_fixed] = c_fixed
     # 2. Generate noises
     
-    dist_e = dist.Normal(torch.tensor([0.]), torch.tensor([sigma_e]))
-    dist_o = dist.Normal(torch.tensor([0.]), torch.tensor([sigma_o]))
-    dist_u = dist.Normal(torch.tensor([0.]), torch.tensor([sigma_u]))
-    dist_r = dist.Normal(torch.tensor([0.]), torch.tensor([sigma_r]))
-    dist_v = dist.Normal(torch.tensor([0.]), torch.tensor([sigma_v]))
-    dist_w = dist.Normal(torch.tensor([0.]), torch.tensor([sigma_w]))
+    dist_e = dist.Normal(torch.DoubleTensor([0.]), torch.DoubleTensor([sigma_e]))
+    dist_o = dist.Normal(torch.DoubleTensor([0.]), torch.DoubleTensor([sigma_o]))
+    dist_u = dist.Normal(torch.DoubleTensor([0.]), torch.DoubleTensor([sigma_u]))
+    dist_r = dist.Normal(torch.DoubleTensor([0.]), torch.DoubleTensor([sigma_r]))
+    dist_v = dist.Normal(torch.DoubleTensor([0.]), torch.DoubleTensor([sigma_v]))
+    dist_w = dist.Normal(torch.DoubleTensor([0.]), torch.DoubleTensor([sigma_w]))
     
     e = dist_e.sample((L,))
     o = dist_o.sample((L,))
@@ -44,18 +44,18 @@ def generative_procedure_with_eval(mu_0, delta_0, gamma_0, sigma_e, sigma_o, sig
     # 3. Generate alpha
     
     mu = []
-    mu_with_initial_values = [torch.tensor(mu_0)]
+    mu_with_initial_values = [torch.DoubleTensor([mu_0])]
     delta = []
-    delta_with_initial_values = [torch.tensor(delta_0)]
+    delta_with_initial_values = [torch.DoubleTensor([delta_0])]
     gamma = []
     gamma_with_initial_values = []
     
-    gamma_vec = [torch.tensor(gamma_0)]
+    gamma_vec = [torch.DoubleTensor(gamma_0)]
     
     
     
     for i in range(len(gamma_0)):
-        gamma_with_initial_values.append(torch.tensor(gamma_0[i]))
+        gamma_with_initial_values.append(torch.DoubleTensor([gamma_0[i]]))
     
              
     for t in range(L):
@@ -93,11 +93,11 @@ def generative_procedure_with_eval(mu_0, delta_0, gamma_0, sigma_e, sigma_o, sig
         
         
     mu = torch.tensor(mu)
-    mu_with_initial_values = torch.tensor(mu_with_initial_values)
+    mu_with_initial_values = torch.DoubleTensor([mu_with_initial_values])
     delta = torch.tensor(delta)
-    delta_with_initial_values = torch.tensor(delta_with_initial_values)
+    delta_with_initial_values = torch.DoubleTensor([delta_with_initial_values])
     gamma = torch.tensor(gamma)
-    gamma_with_initial_values = torch.tensor(gamma_with_initial_values)
+    gamma_with_initial_values = torch.DoubleTensor([gamma_with_initial_values])
     
     gamma_vec = gamma_vec[1:]
 
@@ -120,14 +120,101 @@ def generative_procedure_with_eval(mu_0, delta_0, gamma_0, sigma_e, sigma_o, sig
         
     return (y, mu, delta, gamma, mu_with_initial_values, delta_with_initial_values, gamma_with_initial_values, gamma_vec, z_a, z_c)
         
+def generative_procedure_future(mu_0, delta_0, gamma_0, sigma_e, sigma_o, sigma_u, sigma_r, sigma_v, sigma_w, L, S):
     
+   
+    # 2. Generate noises
+    
+    dist_e = dist.Normal(torch.DoubleTensor([0.]), torch.DoubleTensor([sigma_e]))
+    dist_o = dist.Normal(torch.DoubleTensor([0.]), torch.DoubleTensor([sigma_o]))
+    dist_u = dist.Normal(torch.DoubleTensor([0.]), torch.DoubleTensor([sigma_u]))
+    dist_r = dist.Normal(torch.DoubleTensor([0.]), torch.DoubleTensor([sigma_r]))
+    dist_v = dist.Normal(torch.DoubleTensor([0.]), torch.DoubleTensor([sigma_v]))
+    dist_w = dist.Normal(torch.DoubleTensor([0.]), torch.DoubleTensor([sigma_w]))
+    
+    e = dist_e.sample((L,))
+    o = dist_o.sample((L,))
+    u = dist_u.sample((L,))
+    r = dist_r.sample((L,))
+    v = dist_v.sample((L,))
+    w = dist_w.sample((L,))
+    
+    # 3. Generate alpha
+    
+    mu = []
+    mu_with_initial_values = [torch.DoubleTensor([mu_0])]
+    delta = []
+    delta_with_initial_values = [torch.DoubleTensor([delta_0])]
+    gamma = []
+    gamma_with_initial_values = []
+    
+    
+    
+    
+    for i in range(len(gamma_0)):
+        gamma_with_initial_values.append(torch.DoubleTensor([gamma_0[i]]))
+    
+             
+    for t in range(L):
+        
+        
+        mu_t = mu_with_initial_values[-1] + delta_with_initial_values[-1]
+
+        delta_t = delta_with_initial_values[-1] + v[t]
+        
+
+            
+        
+        mu_t = mu_t + u[t]
+
+            
+        gamma_t = - sum(gamma_with_initial_values[-(S-1):]) + w[t]
+        
+
+        
+        mu.append(mu_t)
+        mu_with_initial_values.append(mu_t)
+        
+        delta.append(delta_t)
+        delta_with_initial_values.append(delta_t)
+        
+        gamma.append(gamma_t)
+        gamma_with_initial_values.append(gamma_t)
+        
+            
+        
+        
+    mu = torch.tensor(mu)
+    mu_with_initial_values = torch.DoubleTensor([mu_with_initial_values])
+    delta = torch.tensor(delta)
+    delta_with_initial_values = torch.DoubleTensor([delta_with_initial_values])
+    gamma = torch.tensor(gamma)
+    gamma_with_initial_values = torch.DoubleTensor([gamma_with_initial_values])
+    
+
+
+    # 4. Generate y
+    
+    y = []
+    
+    for t in range(L):
+        
+        y_t = mu[t] + gamma[t]
+        
+        y_t = y_t + e[t]
+
+            
+        y.append(y_t)
+        
+    y = torch.DoubleTensor(y)
+        
+    return y
         
     
     
 def plot_time_series(y, z_a, z_c):
-    plt.figure()
+    plt.figure(figsize = (20,10))
     plt.plot(np.array(y))
     plt.plot(np.array(torch.nonzero(z_a)),np.array(y[torch.nonzero(z_a)]), '*')
     for c in torch.nonzero(z_c):
-        plt.axvline(x=c, linestyle='--')    
-    
+        plt.axvline(x=c, linestyle='--')   
