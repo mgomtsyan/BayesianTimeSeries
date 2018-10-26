d91e0748610ade9d0cbc004e89cedd2aa5d445a2
dashademidova <35659527+dashademidova@users.noreply.github.com>
2018-10-26 05:32:15 +0300

    Update change_point_and_anomaly.py

--- change_point_and_anomaly.py
+++ change_point_and_anomaly.py
@@ -7,8 +7,8 @@ import pyro.distributions as dist
 
 from torch.distributions.normal import Normal
 
-def update_p_a_t(p_a, sigma_0, sigma_e, y_t, mu_t, gamma_t):
-    distr_1 = dist.Normal(mu_t + gamma_t,sigma_0)
+def update_p_a_t(p_a, sigma_o, sigma_e, y_t, mu_t, gamma_t):
+    distr_1 = dist.Normal(mu_t + gamma_t,sigma_o)
     distr_0 = dist.Normal(mu_t + gamma_t,sigma_e)
     p_a_t_1 = p_a * torch.exp(distr_1.log_prob(y_t))
     p_a_t_0 = (1 - p_a) * torch.exp(distr_0.log_prob(y_t))
@@ -27,21 +27,21 @@ def update_p_c_t(p_c, sigma_r, sigma_u, mu_t, mu_t_prev, delta_t_prev):
     
     return p_c_t
     
-def generate_z_a_t(p_a, sigma_0, sigma_e, y_t, mu_t, gamma_t):
-    p_a_t = update_p_a_t(p_a, sigma_0, sigma_e, y_t, mu_t, gamma_t)
+def generate_z_a_t(p_a, sigma_o, sigma_e, y_t, mu_t, gamma_t):
+    p_a_t = update_p_a_t(p_a, sigma_o, sigma_e, y_t, mu_t, gamma_t)
     distr_1 = dist.Bernoulli(p_a_t)
     
     return distr_1.sample()
     
 def generate_z_c_t(p_c, sigma_r, sigma_u, mu_t, mu_t_prev, delta_t_prev):
-    p_c_t = update_p_a_t(p_c, sigma_r, sigma_u, mu_t, mu_t_prev, delta_t_prev)
+    p_c_t = update_p_c_t(p_c, sigma_r, sigma_u, mu_t, mu_t_prev, delta_t_prev)
     distr_1 = dist.Bernoulli(p_c_t)
     
     return distr_1.sample()
     
 def segment_control(z_c, mu, sigma_r, l):
 
-    t_change_points = torch.nonzero(z_c)[:,0]
+    t_change_points = torch.nonzero(z_c)[:]
     
     distance_between_change_points = t_change_points[1:] - t_change_points[:-1]
   
@@ -68,13 +68,13 @@ def segment_control(z_c, mu, sigma_r, l):
             z_c[t_point_1] = 0
             z_c[t_point_2] = 0
             
-            print('exclude both')
+            #print('exclude both')
             
         else:
             
             i_exclude = np.random.randint(1,3)
             
-            print('exclude', i_exclude)
+            #print('exclude', i_exclude)
             
             if i_exclude == 1:
                 z_c[t_point_1] = 0
